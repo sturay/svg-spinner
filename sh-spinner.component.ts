11c5dc8cdbf366391359ee5b310580a816caedf0
Stuart Harding <stuart_harding@me.com>
2016-09-29 14:15:59 +0100

    Update sh-spinner.component.ts

--- sh-spinner.component.ts
+++ sh-spinner.component.ts
@@ -1,38 +1,29 @@
-/**
-* Author: Stuart Harding
-* stuart_harding@me.com
-* version 0.1
-* initial commit 29 September 2016
-*/
-
-import { Component, ElementRef, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit  } from '@angular/core';
 
 @Component({
-  selector: 'cc-spinner',
-  styleUrls: [ 'sh-spinner.component.css' ],
-  templateUrl: 'sh-spinner.component.html'
+  selector: 'app-sh-spinner',
+  templateUrl: './sh-spinner.component.html',
+  styleUrls: ['./sh-spinner.component.css']
 })
 
-export class ccSpinner implements ElementRef {
+export class ShSpinnerComponent implements ElementRef, OnDestroy, OnInit  {
   nativeElement = {};
   animationIndex = 0;
 
-  constructor(private elementRef:ElementRef){}
-
-  requestAnimationFrame: (callback: () => void) => void = (function(){ 
-    return window.requestAnimationFrame || 
-    (<any>window).webkitRequestAnimationFrame || 
-    (<any>window).mozRequestAnimationFrame || 
-    (<any>window).oRequestAnimationFrame || 
-    (<any>window).window.msRequestAnimationFrame || 
-    function(callback){ 
-        window.setTimeout(callback, 1000 / 60, new Date().getTime()); 
-    }; 
-  })(); 
+  requestAnimationFrame: (callback: () => void) => void = (function () {
+    return window.requestAnimationFrame ||
+      (<any>window).webkitRequestAnimationFrame ||
+      (<any>window).mozRequestAnimationFrame ||
+      (<any>window).oRequestAnimationFrame ||
+      (<any>window).window.msRequestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60, new Date().getTime());
+      };
+  })();
 
   // attributes from html
-  @Input() id: string = this.shape;
   @Input() type: string | undefined = this.type || 'circle';
+  @Input() shape: string = this.shape;
   @Input() opacity: number | undefined = this.opacity || 1;
   @Input() speed: string | undefined = this.speed || '1';
   @Input() diameter: number | undefined = this.diameter || 64;
@@ -44,33 +35,41 @@ export class ccSpinner implements ElementRef {
   originOffset = (this.diameter === 0) ? 32 : this.diameter / 2;
   originOffsetW = this.elemWidth / 2;
   originOffsetH = (this.elemHeight / 2) - (this.originOffset / 2);
+  dims = this.diameter - this.width;
   radius = this.originOffset - ((this.width / 2) + 2);
   reset;
   animationTarget;
   thetaDelta = parseFloat(this.speed) || 2;
 
+  ngOnInit()    { this.doAnim(); }
+  ngOnDestroy() { /* todo */ }
+
   doAnim() {
     switch (this.type) {
       case 'line':
-          this.reset = this.elemWidth;
-          break;
+        this.reset = this.elemWidth;
+        break;
       case 'square':
-          this.reset = this.diameter * 4;
-          break;
+        this.reset = this.diameter * 4;
+        break;
       case 'circle':
-          this.reset = this.diameter * Math.PI;
-          break;
+        this.reset = this.diameter * Math.PI;
+        break;
       case 'surround': // doesn't currently work. Did once, but I broke it.
-          this.reset = (this.elemWidth * 2) + (this.elemHeight * 2);
-          break;
+        this.reset = (this.elemWidth * 2) + (this.elemHeight * 2);
+        break;
     }
     if (this.type === 'circle') {
-        this.animationTarget.setAttribute("transform", "rotate(" + this.animationTarget.currentTheta + ")");
+      this.animationTarget.setAttribute('transform', 'rotate(' + this.animationTarget.currentTheta + ')');
     }
-    this.animationTarget.setAttribute("stroke-dasharray", this.animationTarget.currentTheta);
+    this.animationTarget.setAttribute('stroke-dasharray', this.animationTarget.currentTheta);
     this.animationTarget.currentTheta += this.thetaDelta;
-    if (this.animationTarget.currentTheta >= this.reset) this.animationTarget.currentTheta = 0;
+    if (this.animationTarget.currentTheta >= this.reset) {
+      this.animationTarget.currentTheta = 0;
+    }
     requestAnimationFrame(this.doAnim);
   }
+
+  constructor(private elementRef: ElementRef) { }
 
 }
